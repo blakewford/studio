54da36d89099f7e6ca44aedc425da2f9cdae4181
Blake W. Ford <blakewford@gmail.com>
2018-09-29 13:35:58 -0500

    Add sidebar for program frames

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 SRC   = main.cpp imgui_impl_sdl.cpp imgui_impl_opengl3.cpp imgui.cpp imgui_demo.cpp imgui_draw.cpp imgui_widgets.cpp ./GL/gl3w.c
 OBJS  = $(addsuffix .o, $(basename $(notdir $(SRC))))
 LIBS  = -lGL -ldl `sdl2-config --libs`
-FLAGS = -I. `sdl2-config --cflags` -Wall -Wformat
+FLAGS = -I. `sdl2-config --cflags` -Wall -Wformat -std=c++11 -gdwarf-4
 
 medbench: $(OBJS)
 	$(CXX) -o $@ $^ $(FLAGS) $(LIBS)
@@ -10,7 +10,7 @@ medbench: $(OBJS)
 	$(CXX) $(FLAGS) -c -o $@ $<
 
 %.o:./GL/%.c
-	$(CC) $(FLAGS) -c -o $@ $<
+	$(CC) -I. -c -o $@ $<
 
 clean:
 	rm -f medbench $(OBJS)
--- main.cpp
+++ main.cpp
@@ -1,10 +1,53 @@
 #include <SDL.h>
+#include <fstream>
 #include <stdio.h>
 #include <GL/gl3w.h>
 
 #include "imgui.h"
 #include "imgui_impl_sdl.h"
 #include "imgui_impl_opengl3.h"
+
+const uint32_t SDL_BLACK = 0x00000000;
+const uint32_t SDL_WHITE = 0x00FFFFFF;
+
+struct pgm
+{
+    const char* magic = "P2";
+    const int32_t max = 255;
+    int32_t width, height;
+    uint32_t* image;
+
+    pgm()
+    {
+        width = 0;
+        height = 0;
+        image = nullptr;
+    }
+};
+
+void readPgm(std::ifstream& stream, pgm& image)
+{
+    int32_t max;
+    std::string magic;
+    stream >> magic >> image.width >> image.height >> max;
+
+    int32_t pixel;
+    int32_t width = image.width;
+    int32_t height = image.height;
+    image.image = new uint32_t[image.width*image.height];
+
+    int32_t i = 0;
+    while(height--)
+    {
+        while(width--)
+        {
+            stream >> pixel;
+            image.image[i] = pixel == 0 ? SDL_BLACK: SDL_WHITE;
+            i++;
+        }
+        width = image.width;
+    }
+}
 
 int main(int argc, char** argv)
 {
@@ -22,7 +65,7 @@ int main(int argc, char** argv)
 
     SDL_DisplayMode current;
     SDL_GetCurrentDisplayMode(0, &current);
-    SDL_Window* window = SDL_CreateWindow("MedBench", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE|SDL_WINDOW_BORDERLESS);
+    SDL_Window* window = SDL_CreateWindow("MedBench", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL|SDL_WINDOW_BORDERLESS);
     SDL_GLContext gl_context = SDL_GL_CreateContext(window);
     SDL_GL_SetSwapInterval(1);
 
@@ -38,6 +81,17 @@ int main(int argc, char** argv)
     ImGui::StyleColorsDark();
 
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    pgm texture;
+    std::ifstream imageData;
+    imageData.open("Logo.pgm");
+    readPgm(imageData, texture);
+    imageData.close();
+
+    GLuint textureID = 0;
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 128, 64, 0, GL_RGBA, GL_UNSIGNED_BYTE, texture.image);
+    glGenerateMipmap(GL_TEXTURE_2D);
 
     bool done = false;
     while(!done)
@@ -55,18 +109,21 @@ int main(int argc, char** argv)
         ImGui::NewFrame();
 
         static float f = 0.0f;
-        static int counter = 0;
 
-        ImGui::Begin("Hello, world!");
+        ImGui::Begin("Edit", nullptr, ImGuiWindowFlags_NoResize|ImGuiWindowFlags_NoMove|ImGuiWindowFlags_NoCollapse);
         ImGui::Text("This is some useful text.");
         ImGui::SliderFloat("float", &f, 0.0f, 1.0f);   
         ImGui::ColorEdit3("clear color", (float*)&clear_color);
-        if (ImGui::Button("Button"))
-            counter++;
-        ImGui::SameLine();
-        ImGui::Text("counter = %d", counter);
+        if(ImGui::Button("Button"))
+        {
+            done = true;
+        }
 
         ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        ImGui::End();
+
+        ImGui::Begin("Frames", nullptr, ImGuiWindowFlags_NoResize|ImGuiWindowFlags_NoMove|ImGuiWindowFlags_NoCollapse);
+        ImGui::ImageButton((void*)(intptr_t)textureID, ImVec2(128,64));
         ImGui::End();
 
         ImGui::Render();
@@ -76,6 +133,7 @@ int main(int argc, char** argv)
         glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         SDL_GL_SwapWindow(window);
+        SDL_Delay(10);
     }
 
     ImGui_ImplOpenGL3_Shutdown();
